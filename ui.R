7efc80f89479e109e7159ec4cf378e372701db97
Ruthger Righart <rrighart@googlemail.com>
2017-03-22 21:58:20 +0100

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,13 @@
+library(shiny)
+
+# ui.R
+
+fluidPage(
+    
+  # Copy the line below to make an action button
+  actionButton("action", label = "Action"),
+  
+  hr(),
+  fluidRow(column(2, verbatimTextOutput("value")))
+  )
+
